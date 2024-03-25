cb285ac1802fad5d493b1ebfb864b71e890057ce
Hazel Alain <hazelo235@gmail.com>
2024-03-24 20:07:13 -0600

    Initial Commit

---
+++ architecture Architecture_VIPER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  architecture Architecture_VIPER
+//
+//  Created by Hazel Alain on 24/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ architecture Architecture_VIPERUITests/architecture_Architecture_VIPERUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  architecture_Architecture_VIPERUITestsLaunchTests.swift
+//  architecture Architecture_VIPERUITests
+//
+//  Created by Hazel Alain on 24/03/24.
+//
+
+import XCTest
+
+final class architecture_Architecture_VIPERUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
