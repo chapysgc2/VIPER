98923fe6a4dfc4a31bdc39f5c39ecec54939973b
Hazel Alain <hazelo235@gmail.com>
2024-03-28 20:26:51 -0600

    🔨view and presenter

--- architecture Architecture_VIPER/ListOfMovies/ListOfMoviesPresenter.swift
+++ architecture Architecture_VIPER/ListOfMovies/ListOfMoviesPresenter.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+protocol ListOfMoviesUI : AnyObject {
+    
+    func update(movies : [PopularMovieEntity])
+    
+}
+
+class ListOfMoviesPresenter {
+    var ui : ListOfMoviesUI? //creamos nuestra propiedad ui para poder usar el protocolo ListOfMoviesUI y este sera opcional
+    //Esto va de la mano
+    private let listOfMoviesInteractor : ListOfMoviesInteractor //creamos una referencia o propiedad a nuestro interactor
+    init(listOfMoviesInteractor: ListOfMoviesInteractor) {
+        self.listOfMoviesInteractor = listOfMoviesInteractor
+    }
+    
+    //hasta init
+    
+    //creamos el metodo onViewAppear, este emtodo llamara el interactor para realizar la solicitud http
+    
+    func onViewAppear() {
+        
+        Task {
+            let models = await listOfMoviesInteractor.getListOfMovies() //aqui en la constante models se guarda lo que arroje la solicitud del interactor
+            ui?.update(movies: models.result) //le pasamos el modelo
+        }
+        
+    }
+}
--- architecture Architecture_VIPER/ListOfMovies/ListOfMoviesView.swift
+++ architecture Architecture_VIPER/ListOfMovies/ListOfMoviesView.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import UIKit
+
+
+class ListOfMoviesView : UIViewController {
+    
+    let presenter : ListOfMoviesPresenter?
+    
+    
+    init(presenter: ListOfMoviesPresenter) {
+           self.presenter = presenter
+           super.init(nibName: nil, bundle: nil)
+       }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        presenter?.onViewAppear()
+    }
+}
+
+extension ListOfMoviesView : ListOfMoviesUI {
+    func update(movies: [PopularMovieEntity]) {
+        print("Datos recibidos \(movies)")
+    }
+}
